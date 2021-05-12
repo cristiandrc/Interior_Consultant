e83754098a6a25b4452c6f8a837614a3802d97f1
cristian <crisdarroca@hotmail.com>
2021-05-11 20:18:13 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const BTN_MENU = document.querySelector("#btn_menu")
+const NAV_MENU = document.querySelector("#nav_menu")
+
+const menu = () => {
+    // alert('hola')
+    NAV_MENU.display = 'block'
+}
+
+
+BTN_MENU.addEventListener('click', menu)
+
